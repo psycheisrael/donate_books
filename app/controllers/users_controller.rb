e8b168d72a7ea977c3f31db23c661ccab5b8807c
psycheisrael <mudyaisrael@gmail.com>
2020-05-17 18:15:07 -0400

    commit to git

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find(params[:user_id])
     end
 
     # Only allow a list of trusted parameters through.
