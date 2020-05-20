de72f15c84c51631e737b29ef0d3f5db0a519fff
psycheisrael <mudyaisrael@gmail.com>
2020-05-20 17:21:22 -0400

    user page edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,4 @@ class User < ApplicationRecord
   has_many :books
   validates :full_name, presence: true, uniqueness: true
   validates :email, presence: true
-  validates :number_of_books, presence: true
-  validates :title, presence: true
-  validates :brief_prolong, presence: true
 end
