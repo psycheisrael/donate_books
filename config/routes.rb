05f7be8116e809b1041ec924cd63738c93c6e71e
psycheisrael <mudyaisrael@gmail.com>
2020-05-20 18:58:59 -0400

    commit to git

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/inddex'
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "books#index"
 end
--- test/controllers/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get inddex" do
-    get home_inddex_url
-    assert_response :success
-  end
-
-end
