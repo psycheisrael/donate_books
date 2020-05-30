3faf7dad6c83c194a5e862cfe357f9b472a1388b
psycheisrael <mudyaisrael@gmail.com>
2020-05-29 23:16:23 -0400

    styling fix

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,11 @@ ActiveRecord::Schema.define(version: 2020_05_17_122220) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
